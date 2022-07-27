a6abfa99bc57bdc4341e938d5229fa641aaa334e
eddisonchiek <21034014@myrp.edu.sg>
2022-07-27 22:19:29 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/sg/c346/id21034014/ndpsong/Activity.java
@@ -0,0 +1,14 @@
+package sg.edu.sg.c346.id21034014.ndpsong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_);
+    }
+}
---
+++ app/src/main/java/sg/edu/sg/c346/id21034014/ndpsong/Test.java
@@ -0,0 +1,4 @@
+package sg.edu.sg.c346.id21034014.ndpsong;
+
+public class Test {
+}
